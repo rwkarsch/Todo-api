5dcf0dd9e3e98f5fd333cbb541f0efd5c94254e8
rwkarsch <robertkarsch@gmail.com>
2016-10-03 06:17:52 -0400

    added the underscore library and functionality

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -19,16 +20,9 @@ app.get('/todos', function(req, res){
 
 //GET /todos/:id
 app.get('/todos/:id', function (req, res){
-	// params are a string, so we need to move it to an integer
+// params are a string, so we need to move it to an integer
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
-
-	todos.forEach (function (todo) {
-		if (todo.id === todoId) {
-			matchedTodo = todo;
-			
-		}
-	});
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
@@ -40,10 +34,16 @@ app.get('/todos/:id', function (req, res){
 
 //POST /todos
 app.post('/todos', function (req, res){
-	var body = req.body;
-	// add id field
+	
+	var body = _.pick(req.body, 'description', 'completed');
+	
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	};
+	// set body.description to trimmed 
+	body.description = body.description.trim();
+
 	body.id = todoNextId++;
-	console.log(todoNextId);
 	// push body into array
 	todos.push(body);
 
