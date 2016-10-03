1a411363108e8eddfec2dd605cad946016ff946a
rwkarsch <robertkarsch@gmail.com>
2016-10-03 05:20:25 -0400

    Add POST for todos

--- server.js
+++ server.js
@@ -1,22 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [
-{
-	id: 1,
-	description: 'Meet Mom for lunch',
-	completed: false
-}, 
-{
-	id: 2,
-	description: 'Go to market',
-	completed: false
-},
-{
-	id: 3,
-	description: 'Get mother in law gift',
-	completed: true
-}]
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root')
@@ -29,6 +19,7 @@ app.get('/todos', function(req, res){
 
 //GET /todos/:id
 app.get('/todos/:id', function (req, res){
+	// params are a string, so we need to move it to an integer
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo;
 
@@ -46,6 +37,21 @@ app.get('/todos/:id', function (req, res){
 	}
 
 })
+
+//POST /todos
+app.post('/todos', function (req, res){
+	var body = req.body;
+	// add id field
+	body.id = todoNextId++;
+	console.log(todoNextId);
+	// push body into array
+	todos.push(body);
+
+//	console.log('description: ' + body.description);
+
+	res.json(body);
+});
+
 
 app.listen(PORT, function () {
 	console.log('Express running on port ' + PORT + '!')
