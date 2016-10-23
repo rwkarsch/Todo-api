612b99250f29a66f2d607992835844e9c80d6e23
rwkarsch <robertkarsch@gmail.com>
2016-10-23 19:42:20 -0400

    Add sequalize demo

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -20,29 +20,46 @@ var Todo = sequelize.define('todo', {
 });
 
 sequelize.sync({
-	force: true
+//	force: true
 }).then(function () {
 	console.log('Everything is synced');
 
-
-
-	Todo.create({
-  		description: 'Take out trash',
-		completed: false
-	}).then(function (todo){
-		return Todo.create({
-			description: 'Clean office'
-		})
-	}).then (function(){
-		return Todo.findById(1)
+	return Todo.findById(4);
+	
 	}).then (function (todo){
-		if(todo) {
+		if(todo){
 			console.log(todo.toJSON());
 		} else {
-			console.log('no todo found!');
+			console.log('No todo found!');
 		}
-	}).catch(function (e) {
-		console.log(e);
 	});
+
+	// Todo.create({
+ //  		description: 'Take out trash',
+	// 	completed: false
+	// }).then(function (todo){
+	// 	return Todo.create({
+	// 		description: 'Clean office'
+	// 	})
+	// }).then (function(){
+	// 	//return Todo.findById(1)
+	// 	return Todo.findAll({
+	// 		where: {
+	// 			description: {
+
+	// 				$like: '%Office%'}
+	// 		}
+	// 	})
+	// }).then (function (todos){
+	// 	if(todos) {
+	// 		todos.forEach(function (todo){
+
+	// 			console.log(todo.toJSON());
+	// 		});
+	// 	} else {
+	// 		console.log('no todo found!');
+	// 	}
+	// }).catch(function (e) {
+	// 	console.log(e);
+	// });
 	
-});
