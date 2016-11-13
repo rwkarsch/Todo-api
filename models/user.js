2725e046315e20f3fbdc60cf36d13f83bfc74f3b
rwkarsch <robertkarsch@gmail.com>
2016-11-13 08:02:04 -0500

    Hash and salt passwords

--- models/user.js
+++ models/user.js
@@ -1,18 +1,35 @@
-module.exports = function (sequelize, Datatypes) {
+var bcrypt = require('bcryptjs');
+var _ = require('underscore');
+
+module.exports = function (sequelize, DataTypes) {
 	return sequelize.define('user', {
 		email: {
-			type: Datatypes.STRING,
+			type:  DataTypes.STRING,
 			allowNull: false,
 			unique: true,
 			validate: {
 				isEmail: true
 			} 
 		},
+		salt: {
+			type: DataTypes.STRING
+		},
+		password_hash: {
+			type: DataTypes.STRING
+		},
 		password: {
-			type: Datatypes.STRING,
+			type:  DataTypes.VIRTUAL,
 			allowNull: false,
 			validate: {
 				len: [7, 100]
+			},
+			set: function (value){
+				var salt = bcrypt.genSaltSync(10);
+				var hashedPassword = bcrypt.hashSync(value, salt);
+
+				this.setDataValue('password', value);
+				this.setDataValue('salt', salt);
+				this.setDataValue('password_hash', hashedPassword)
 			}
 		}
 	} , {
@@ -22,6 +39,12 @@ module.exports = function (sequelize, Datatypes) {
 				if (typeof user.email === 'string') {
 					user.email = user.email.toLowerCase();
 				}
+			}
+		}, 
+		instanceMethods: {
+			toPublicJSON: function () {
+				var json = this.toJSON();
+				return _.pick(json, 'email', 'id', 'createdAt', 'updatedAt');
 			}
 		}
 	});
--- server.js
+++ server.js
@@ -145,7 +145,7 @@ app.post('/users', function(req, res) {
 	body.password = body.password.trim();
 
 	db.user.create(body).then(function (user){
-		res.json(user.toJSON());
+		res.json(user.toPublicJSON());
 	}, function (e){
 		res.status(400).json(e);
 	})
